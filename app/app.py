d33c4fb0be905711f437129d7dda3152a2ed1847
Zakhar Petukhov <zakharpetukhov@protonmail.com>
2019-03-27 20:22:22 +0300

    circular importing removed

--- app/app.py
+++ app/app.py
@@ -1,8 +1,9 @@
 from config import Configuration
 from flask import Flask
-from posts.blueprint import posts
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object(Configuration)
 
-app.register_blueprint(posts, url_prefix='/blog')
+db = SQLAlchemy(app)
+
