880402ac8e992d7705eb2607dfed6fb6cdc25ecd
Zakhar Petukhov <zakharpetukhov@protonmail.com>
2019-03-27 23:14:16 +0300

    tag distribution

--- app/posts/blueprint.py
+++ app/posts/blueprint.py
@@ -1,6 +1,6 @@
 from flask import Blueprint
 from flask import render_template
-from models import Post
+from models import Post, Tag
 
 posts = Blueprint('posts', __name__, template_folder='templates')
 
@@ -14,4 +14,12 @@ def index():
 @posts.route('/<slug>')
 def post_detail(slug):
     post = Post.query.filter(Post.slug == slug).first()
-    return render_template('posts/post_detail.html', post=post)
+    tags = post.tags
+    return render_template('posts/post_detail.html', post=post, tags=tags)
+
+
+@posts.route('/tag/<slug>')
+def tag_detail(slug):
+    tag = Tag.query.filter(Tag.slug == slug).first()
+    posts = tag.posts.all()
+    return render_template('posts/tag_detail.html', tag=tag, posts=posts)
