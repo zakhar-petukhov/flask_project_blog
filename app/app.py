226239385dcf66f79c954423c3353885c720762b
Zakhar Petukhov <zakharpetukhov@protonmail.com>
2019-03-27 23:07:12 +0300

    add migrate

--- app/app.py
+++ app/app.py
@@ -1,9 +1,14 @@
 from config import Configuration
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager
 
 app = Flask(__name__)
 app.config.from_object(Configuration)
 
 db = SQLAlchemy(app)
 
+migrate = Migrate(app, db)
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
