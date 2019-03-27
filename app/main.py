afa870bc3253ab7d881e8a816afb633707bd7df9
Zakhar Petukhov <zakharpetukhov@protonmail.com>
2019-03-27 20:29:37 +0300

    register blueprint

---
+++ app/main.py
@@ -0,0 +1,11 @@
+from app import app
+from app import db
+import view
+from posts.blueprint import posts
+
+
+app.register_blueprint(posts, url_prefix='/blog')
+
+
+if __name__ == '__main__':
+    app.run(port=8080)
