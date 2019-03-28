4093313bf9b63dc9a1494fc16586412d5f2d40b0
Zakhar Petukhov <zakharpetukhov@protonmail.com>
2019-03-29 00:06:14 +0300

    connection admin pannel

--- app/app.py
+++ app/app.py
@@ -1,8 +1,10 @@
 from config import Configuration
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object(Configuration)
@@ -12,3 +14,9 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
+
+
+from models import *
+admin = Admin(app)
+admin.add_view(ModelView(Post, db.session))
+admin.add_view(ModelView(Tag, db.session))
