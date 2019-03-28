af0ce0627742bf6d616470943623f96a65bc6295
Zakhar Petukhov <zakharpetukhov@protonmail.com>
2019-03-28 23:22:39 +0300

    add variable for pagination

--- app/posts/blueprint.py
+++ app/posts/blueprint.py
@@ -33,12 +33,20 @@ def create_post():
 def index():
     q = request.args.get('q')
 
+    page = request.args.get('page')
+
+    if page and page.isdigit():
+        page = int(page)
+    else:
+        page = 1
+
     if q:
-        posts = Post.query.filter(Post.title.contains(q) | Post.body.contains(q)).all()
+        posts = Post.query.filter(Post.title.contains(q) | Post.body.contains(q))
     else:
         posts = Post.query.order_by(Post.created.desc())
 
-    return render_template('posts/index.html', posts=posts)
+    pages = posts.paginate(page=page, per_page=5)
+    return render_template('posts/index.html', pages=pages)
 
 
 @posts.route('/<slug>')
