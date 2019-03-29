1e33a829ba96f207695b88f9dafd7a30f1345d69
Zakhar Petukhov <zakharpetukhov@protonmail.com>
2019-03-30 00:03:30 +0300

    base view

---
+++ app/view.py
@@ -0,0 +1,12 @@
+from app import app
+from flask import render_template
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.errorhandler(404)
+def page_not_dound(e):
+    return render_template('404.html'), 404
