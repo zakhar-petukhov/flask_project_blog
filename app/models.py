e940b09d8d1782dd4b7ea3d545bf6ad3115b65ef
Zakhar Petukhov <zakharpetukhov@protonmail.com>
2019-03-29 23:32:38 +0300

    create class user-elevated rights and role

--- app/models.py
+++ app/models.py
@@ -1,6 +1,9 @@
-from app import db
-from datetime import datetime
 import re
+from datetime import datetime
+
+from flask_security import UserMixin, RoleMixin
+
+from app import db
 
 
 def slugify(s):
@@ -44,5 +47,31 @@ class Tag(db.Model):
         super(Tag, self).__init__(*args, **kwargs)
         self.slug = slugify(self.name)
 
+    def generate_slug(self):
+        if self.name:
+            self.slug = slugify(self.name)
+
     def __repr__(self):
         return '<Tag id: {}, name: {}>'.format(self.id, self.name)
+
+
+####### Flask Security #######
+
+roles_users = db.Table('roles_users',
+                       db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
+                       db.Column('role_id', db.Integer(), db.ForeignKey('role.id'))
+                       )
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer(), primary_key=True)
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(255))
+    active = db.Column(db.Boolean)
+    roles = db.relationship('Role', secondary=roles_users, backref=db.backref('users', lazy='dynamic'))
+
+
+class Role(db.Model, RoleMixin):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(100), unique=True)
+    description = db.Column(db.String(255))
