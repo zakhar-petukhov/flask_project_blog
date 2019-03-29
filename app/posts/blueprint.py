e875989086c7ccf0a83f66b225c5bc235037629f
Zakhar Petukhov <zakharpetukhov@protonmail.com>
2019-03-29 23:34:20 +0300

    add decorator for check register users

--- app/posts/blueprint.py
+++ app/posts/blueprint.py
@@ -1,16 +1,19 @@
 from flask import Blueprint
-from flask import render_template
-from models import Post, Tag
-from flask import request
-from .forms import PostForm
-from app import db
 from flask import redirect
+from flask import render_template
+from flask import request
 from flask import url_for
+from flask_security import login_required
+from models import Post, Tag
+
+from app import db
+from .forms import PostForm
 
 posts = Blueprint('posts', __name__, template_folder='templates')
 
 
 @posts.route('/create', methods=['POST', 'GET'])
+@login_required
 def create_post():
     if request.method == 'POST':
         title = request.form['title']
@@ -30,6 +33,7 @@ def create_post():
 
 
 @posts.route('<slug>/edit/', methods=['POST', 'GET'])
+@login_required
 def edit_post(slug):
     post = Post.query.filter(Post.slug == slug).first()
 
